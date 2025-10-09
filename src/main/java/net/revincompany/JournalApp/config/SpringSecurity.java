315d3d696489086526314f4454f7754655e16724
revin99 <revingohil@gmail.com>
2025-10-09 22:47:31 +0530

    Adding admin CRUDs and added admin authentication

--- src/main/java/net/revincompany/JournalApp/config/SpringSecurity.java
+++ src/main/java/net/revincompany/JournalApp/config/SpringSecurity.java
@@ -26,6 +26,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/journals/**,/user/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
