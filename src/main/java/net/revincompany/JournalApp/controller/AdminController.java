f1b7219369c4e26155746fd28064417b985fbaa2
revin99 <revingohil@gmail.com>
2025-10-09 22:49:15 +0530

    Adding admin CRUDs and added admin authentication

--- src/main/java/net/revincompany/JournalApp/controller/AdminController.java
+++ src/main/java/net/revincompany/JournalApp/controller/AdminController.java
@@ -5,9 +5,7 @@ import net.revincompany.JournalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,10 @@ public class AdminController {
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
+    }
+
+    @PostMapping("/create-admin-role")
+    public void createUser(@RequestBody User user){
+        userService.saveAdminEntry(user);
     }
 }
