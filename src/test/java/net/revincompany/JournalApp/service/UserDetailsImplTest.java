2fab0b12898ba1dba9d6fc17247f614de807ce86
revin99 <revingohil@gmail.com>
2025-10-11 21:55:22 +0530

    Mockito trial with MockBean

---
+++ src/test/java/net/revincompany/JournalApp/service/UserDetailsImplTest.java
@@ -0,0 +1,29 @@
+package net.revincompany.JournalApp.service;
+
+import net.revincompany.JournalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class UserDetailsImplTest {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    void loadUserByUserNameTest(){
+        when(userRepository.findByUserName("ram")).thenReturn((net.revincompany.JournalApp.entity.User) User.builder().username("ram").password("ram").build());
+        UserDetails user = userDetailsService.loadUserByUsername("ram");
+        Assertions.assertNotNull(user);
+    }
+}
