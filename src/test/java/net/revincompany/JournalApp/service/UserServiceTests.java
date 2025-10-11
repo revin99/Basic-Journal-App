5de5a5b4a8200a59f5bcb9f76312740341edcfe7
revin99 <revingohil@gmail.com>
2025-10-11 21:40:14 +0530

    Basic Junit testing

---
+++ src/test/java/net/revincompany/JournalApp/service/UserServiceTests.java
@@ -0,0 +1,59 @@
+package net.revincompany.JournalApp.service;
+
+import net.revincompany.JournalApp.entity.User;
+import net.revincompany.JournalApp.repository.UserRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void testFindByUserName(){
+
+        //assertEquals(4,2+2);
+        assertNotNull(userRepository.findByUserName("revin"));
+    }
+
+    @Disabled
+    @Test
+    public void testCheckJournalEntries(){
+        User user = userRepository.findByUserName("revin");
+        assertTrue(user.getJournalEntries().isEmpty());
+    }
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,10,12",
+            "3,3,9"
+    })
+    public void testAdd(int a, int b, int expected)
+    {
+        assertEquals(expected,a+b);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings={
+            "ram",
+            "revin",
+            "shyam"
+    })
+    public void testFindByUserNameParam(String name)
+    {
+        assertNotNull(userRepository.findByUserName(name));
+    }
+}
